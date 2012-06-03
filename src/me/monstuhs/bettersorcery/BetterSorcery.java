759cc35980cea3e5075991ba07d6122d8d6835b2
James <jamestwilson@live.com>
2012-06-03 16:28:33 -0700

    Initial add

---
+++ src/me/monstuhs/bettersorcery/BetterSorcery.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.monstuhs.bettersorcery;
+
+/**
+ *
+ * @author James
+ */
+public class BetterSorcery {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
